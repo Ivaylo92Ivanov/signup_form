7df074f44c55ba951be1a71dc974bb3722701aed
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-07-08 10:18:13 +0300

    Create basic password and confirm password validator

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const submitButton = document.querySelector("#submit");
-submitButton.addEventListener("click", () => validatePassword());
-
 function validatePassword() {
     let pass1 = document.querySelector("#pass1").value;
     let pass2 = document.querySelector("#pass2").value;
+    console.log("pressed")
     if(pass1 !== pass2) {
         alert("passwords do not match")
+        return false
+    } else {
+        alert("password is matching")
+        return true
     }
-    console.log(pass1);
-    console.log(pass2);
 }
