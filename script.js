174521a8363be418baf906b7baec7a77a2ca18fd
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-07-12 14:34:32 +0300

    Add green/red options forpasswordSetOutline func

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 function validatePassword() {
     let pass1 = document.querySelector("#pass1").value;
     let pass2 = document.querySelector("#pass2").value;
+    let error = document.querySelector(".error");
     if(pass1 !== pass2) {
-        alert("passwords do not match")
+        // alert("passwords do not match")
+        passwordSetOutline("red")
+        error.style.display = "block";
         return false
     } else {
-        passwordSetOutline()
+        passwordSetOutline("green")
+        error.style.display = "none";
         return true
     }
 }
 
-function passwordSetOutline() {
+function passwordSetOutline(color) {
     let pass1 = document.querySelector("#pass1");
     let pass2 = document.querySelector("#pass2");
-    pass1.style.outline = "3px solid green";
-    pass2.style.outline = "3px solid green";
+    pass1.style.outline = `3px solid ${color}`;
+    pass2.style.outline = `3px solid ${color}`;
     return
 }
