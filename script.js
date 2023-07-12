ff28365bbadc60ebdd89be3c2b0f00370227365c
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-07-12 14:18:04 +0300

    Remove console.log statements

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function validatePassword() {
     let pass1 = document.querySelector("#pass1").value;
     let pass2 = document.querySelector("#pass2").value;
-    console.log("pressed")
     if(pass1 !== pass2) {
         alert("passwords do not match")
         return false
     } else {
         passwordSetOutline()
-        console.log("why not submit")
         return true
     }
 }
